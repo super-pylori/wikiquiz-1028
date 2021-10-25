d1a596fa1953c1b1ce61ec964c1fdf7b71f3054e
super-pylori <super.pylori6@gmail.com>
2021-10-25 22:47:59 +0900

    add json

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Data from './data.json'; // 追加
+
+const wikiList = Data.data.wiki;
 
 function App() {
   return (
@@ -8,15 +11,37 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {wikiList[0].text}
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={wikiList[0].url}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          答えはこちら
+        </a>
+        <p>
+          {wikiList[1].text}
+        </p>
+        <a
+          className="App-link"
+          href={wikiList[1].url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          答えはこちら
+        </a>
+        <p>
+          {wikiList[3].text}
+        </p>
+        <a
+          className="App-link"
+          href={wikiList[3].url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          答えはこちら
         </a>
       </header>
     </div>
